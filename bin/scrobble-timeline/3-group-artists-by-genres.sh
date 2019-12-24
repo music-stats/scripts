df3954385fe2785b9c627fcec8fff1956499058b
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2019-12-24 19:52:44 +0100

    start preparing a grouping of artists by genres

---
+++ bin/scrobble-timeline/3-group-artists-by-genres.sh
@@ -0,0 +1,3 @@
+#!/usr/local/bin/bash
+export NODE_PATH=./dist/
+node ./dist/src/scripts/scrobble-timeline/3-group-artists-by-genres $@
--- src/ETL/extractors/dataset.ts
+++ src/ETL/extractors/dataset.ts
@@ -4,7 +4,7 @@ import {Artist as LastfmArtist} from 'src/types/lastfm';
 import {PackedArtist} from 'src/types/artist';
 
 import config from 'src/config';
-import {readJsonFile} from 'src/utils/file';
+import {readJsonFile, readTxtMultilineFolder, TxtMultilineFolderContent} from 'src/utils/file';
 
 export function loadCountryCodeDataset(): Promise<CountryCodeDataset> {
   return readJsonFile(config.datasets.countryCodes.filePath);
@@ -20,4 +20,8 @@ export function loadLastfmArtistList(): Promise<LastfmArtist[]> {
 
 export function loadMergedArtistList(): Promise<PackedArtist[]> {
   return readJsonFile(config.scripts.artistAreaMap.mergeArtists.outputFilePath);
+}
+
+export function loadArtistListByGenreDataset(): Promise<TxtMultilineFolderContent> {
+  return readTxtMultilineFolder('');
 }
