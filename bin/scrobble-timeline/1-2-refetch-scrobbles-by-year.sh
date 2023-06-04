11ba6c9113ec28a651e14ecfd6159ebb4c1be420
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2023-06-04 17:32:47 +0300

    add a script for refetching all scrobbles for a given year

---
+++ bin/scrobble-timeline/1-2-refetch-scrobbles-by-year.sh
@@ -0,0 +1,5 @@
+rm output/scrobble-timeline/1-scrobbles/$@-01-01--$@-*
+npm run script:scrobble-timeline:1-fetch-scrobbles $@-01-01
+npm run script:scrobble-timeline:2-merge-scrobbles
+npm run script:scrobble-timeline:3-group-artists-by-genres
+npm run script:scrobble-timeline:4-copy-output
