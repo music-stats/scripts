a46e8d36bb4590d6c01e7b68333fcc9ad51d4b46
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2024-02-17 20:36:42 +0200

    fix scripts paths

--- bin/scrobble-timeline/1-2-refetch-scrobbles-by-year.sh
+++ bin/scrobble-timeline/1-2-refetch-scrobbles-by-year.sh
@@ -1,5 +1,5 @@
-rm output/scrobble-timeline/1-scrobbles/$@-01-01--$@-*
-npm run script:scrobble-timeline:1-fetch-scrobbles $@-01-01
+rm output/scrobble-timeline/1-scrobbles/$@-01-01--$@-12-31
+npm run script:scrobble-timeline:1-fetch-scrobbles $@-01-01 $@-12-31
 npm run script:scrobble-timeline:2-merge-scrobbles
 npm run script:scrobble-timeline:3-group-artists-by-genres
 npm run script:scrobble-timeline:4-copy-output
