50a2ec05cc8017d002912bd74536bc6a66d02048
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2024-02-25 12:27:38 +0200

    add more artists to genres

--- bin/scrobble-timeline/4-copy-output.sh
+++ bin/scrobble-timeline/4-copy-output.sh
@@ -10,6 +10,6 @@ cp ./output/scrobble-timeline/1-scrobbles/2020-01-01--2020-12-31.json ../timelin
 cp ./output/scrobble-timeline/1-scrobbles/2021-01-01--2021-12-31.json ../timeline/data/years/2021.json
 cp ./output/scrobble-timeline/1-scrobbles/2022-01-01--2022-12-31.json ../timeline/data/years/2022.json
 cp ./output/scrobble-timeline/1-scrobbles/2023-01-01--2023-12-31.json ../timeline/data/years/2023.json
-cp ./output/scrobble-timeline/1-scrobbles/2024-01-01--2024-*.json ../timeline/data/years/2024.json
+cp ./output/scrobble-timeline/1-scrobbles/2024-01-01--2024-12-31.json ../timeline/data/years/2024.json
 cp ./output/scrobble-timeline/2-merged-scrobbles.json ../timeline/data/years/all.json
 cp ./output/scrobble-timeline/3-artists-by-genres.json ../timeline/data/artists-by-genres.json
